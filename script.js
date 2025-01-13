06a477b588ba731e1398d3af97ca8a009f123718
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-01-14 04:31:14 +0800

    feat: add button to reset grid to desired size

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const main = document.querySelector('.main');
-const parent = document.querySelector('#parent');
-
+const mainCont = document.querySelector('#mainCont');
 
 let x = 16;
-let y = 16;
+
 function createRow(x) {
   for ( let i=0 ; i < x; i++) {
     
     const div = document.createElement('div');
-    parent.appendChild(div);
+    mainCont.appendChild(div);
     div.classList.add('main');
   } 
 }
@@ -18,7 +17,7 @@ createRow (x);
 const mains = document.querySelectorAll('.main');
 
 mains.forEach((createCol) => {
-  for ( let i=0 ; i < y; i++) {
+  for ( let i=0 ; i < x; i++) {
   const div = document.createElement('div');
     createCol.appendChild(div);
     div.classList.add('grid');
@@ -39,3 +38,47 @@ grid.forEach (grids => {
     console.log(`rgb(${r},${g},${b})`);
   })
 });
+
+function gridSize () {
+  let x = document.querySelector('#numb').value;
+  if (x > 100 || isNaN(x)) {
+    document.querySelector('.textOutput').textContent = 'Input a number less than 100';
+  } else {
+    return x;
+  }
+}
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+  const mains = document.querySelectorAll('.main');
+  let val = mains.length
+
+    for ( let i=0 ; i < val; i++) {
+    deleteChild();
+      
+  }
+  
+  let y = gridSize ();
+  createRow (y);
+  
+  const col = document.querySelectorAll('.main');
+  let x = col.length;
+  
+  col.forEach((createCol) => {
+    for ( let i=0 ; i < x; i++) {
+    const div = document.createElement('div');
+      createCol.appendChild(div);
+      div.classList.add('grid');
+    }
+  });
+  
+})
+
+function deleteChild() {
+  const mainCont = document.querySelector('#mainCont');
+  const main = document.querySelector('.main');
+  mainCont.removeChild(main);
+}
+
+
