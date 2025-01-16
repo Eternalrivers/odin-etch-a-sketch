6d06a184ccef2d57e7094d81ec97b38894fb2a80
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-01-16 21:58:20 +0800

    feat: Add opacity effect to the pixels

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const main = document.querySelector('.main');
 const mainCont = document.querySelector('#mainCont');
 
+let mouseClicked = false;
+mainCont.addEventListener ('mousedown', (e) => {
+  mouseClicked = true;
+  console.log(mouseClicked);
+  
+  //stops the event from propagating
+  e.preventDefault();
+});
+
+mainCont.addEventListener ('mouseup', () => {
+  mouseClicked = false;
+});
+
+//Initial grid size during load up.
 let x = 16;
 
 function createRow(x) {
@@ -28,14 +42,22 @@ const grid = document.querySelectorAll('.grid');
 
 grid.forEach (grids => {
   grids.addEventListener('mouseenter', (e) => {
- 
+   if (mouseClicked) {
     const r =  Math.floor(Math.random()*255);
     const g =  Math.floor(Math.random()*255);
     const b =  Math.floor(Math.random()*255);
-
+    
     e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
-
-    console.log(`rgb(${r},${g},${b})`);
+    console.log (e.target.style.backgroundColor);
+    
+    let opacity = Number(window.getComputedStyle(e.target).opacity);
+    console.log(opacity);
+    console.log(e.target.style.opacity);
+      
+      if (opacity < 1) {
+        e.target.style.opacity = opacity + 0.1;
+      }
+    }
   })
 });
 
@@ -77,14 +99,23 @@ btn.addEventListener('click', () => {
 
   grid.forEach (grids => {
     grids.addEventListener('mouseenter', (e) => {
-
-    const r =  Math.floor(Math.random()*255);
+      if (mouseClicked) {
+        const r =  Math.floor(Math.random()*255);
     const g =  Math.floor(Math.random()*255);
     const b =  Math.floor(Math.random()*255);
 
     e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
 
     console.log(`rgb(${r},${g},${b})`);
+
+    let opacity = Number(window.getComputedStyle(e.target).opacity);
+    console.log(opacity);
+      
+      if (opacity < 1) {
+        e.target.style.opacity = opacity + 0.1;
+      }
+      }
+    
   })
 });
 })
